0c97b00c39a69a2d4011d5306f60b5e95f17a521
diegorodolfofranco <d.franco2@campus.unimib.it>
2022-12-13 12:06:08 +0100

    Updated movePlayer Sequence Diagram

---
+++ src/test/java/org/thegoosegame/game/GameTest.java
@@ -0,0 +1,72 @@
+package org.thegoosegame.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void newTurnTest() {
+        boolean isEnded;
+        Player player = new Player("Pippo");
+
+        assertTrue(isEnded == false);
+    }
+
+    @Test
+    void createPlayerTest() {
+    }
+
+    @Test
+    void listPlayersTest() {
+    }
+
+    @Test
+    void playerCheckTest() {
+    }
+
+    @Test
+    void initializeBoardTest() {
+    }
+
+    @Test
+    void addPlayerTest() {
+    }
+
+    @Test
+    void getCellOccupantsTest() {
+    }
+
+    @Test
+    void getPlayersTest() {
+    }
+
+    @Test
+    void setPlayersTest() {
+    }
+
+    @Test
+    void getCellsTest() {
+    }
+
+    @Test
+    void setCellsTest() {
+    }
+
+    @Test
+    void isEndedTest() {
+    }
+
+    @Test
+    void setEndedTest() {
+    }
+
+    @Test
+    void getWinnerTest() {
+    }
+
+    @Test
+    void setWinnerTest() {
+    }
+}
