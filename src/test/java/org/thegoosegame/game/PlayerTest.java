5ca2f7f8aa7c8c199115eeaf92eaa53a1ed45c2c
diegorodolfofranco <d.franco2@campus.unimib.it>
2022-12-13 14:38:21 +0100

    Updated code

---
+++ src/test/java/org/thegoosegame/game/PlayerTest.java
@@ -0,0 +1,11 @@
+package org.thegoosegame.game;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class PlayerTest {
+
+}
