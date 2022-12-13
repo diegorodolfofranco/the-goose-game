ea34ae911f332fc90fb3e773dde002c201dfeb18
diegorodolfofranco <d.franco2@campus.unimib.it>
2022-12-13 14:39:34 +0100

    Updated code

--- src/test/java/org/thegoosegame/game/PlayerTest.java
+++ src/test/java/org/thegoosegame/game/PlayerTest.java
@@ -8,4 +8,75 @@ import static org.mockito.Mockito.*;
 
 class PlayerTest {
 
+    @Test
+    void rollDices() {
+    }
+
+    @Test
+    void moveForward() {
+    }
+
+    @Test
+    void bounce() {
+    }
+
+    @Test
+    void showPlayerMovement() {
+    }
+
+    @Test
+    void getGame() {
+    }
+
+    @Test
+    void setGame() {
+    }
+
+    @Test
+    void getPrevPosition() {
+    }
+
+    @Test
+    void setPrevPosition() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void getPosition() {
+    }
+
+    @Test
+    void setPosition() {
+    }
+
+    @Test
+    void isHasWon() {
+    }
+
+    @Test
+    void setHasWon() {
+    }
+
+    @Test
+    void getFirstDice() {
+    }
+
+    @Test
+    void setFirstDice() {
+    }
+
+    @Test
+    void getSecondDice() {
+    }
+
+    @Test
+    void setSecondDice() {
+    }
 }
